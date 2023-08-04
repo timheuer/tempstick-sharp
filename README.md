2d1528dfd1de471972aa7249ca6162e395002192
Tim Heuer <tim@timheuer.com>
2023-08-03 23:52:02 -0700

    Initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# TempStick#
+A simple C# client for the [TempStick](https://www.tempstick.com/) USB temperature sensor API.
+
+## Installation
+The TempStick library is available as a [NuGet package](https://www.nuget.org/packages/TimHeuer.TempStickSharp/).
+To install TempStickSharp, run the following command in the [Package Manager Console](https://docs.nuget.org/docs/start-here/using-the-package-manager-console)
+
+	PM> Install-Package TimHeuer.TempStickSharp
+
+## License
+TempStickSharp is licensed under the MIT License
+
+## Icon 
+Thermometer by Anggara Putra from <a href="https://thenounproject.com/browse/icons/term/thermometer/" target="_blank" title="Thermometer Icons">Noun Project</a> (CC BY 3.0)
---
+++ src/ApiTests/SensorTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ApiTests;
+
+[TestClass]
+public class SensorTests
+{
+    string API_KEY = string.Empty;
+    string SENSOR_ID = string.Empty;
+    TempStick.TempStickClient client;
+
+    [TestInitialize]
+    public void SetupTests()
+    {
+        var builder = new ConfigurationBuilder()
+            .AddUserSecrets("5bcb7714-77a2-46aa-8937-fbcf172873c7");
+
+        IConfiguration config = builder.Build();
+
+        // get api key from environment variable
+        API_KEY = config["API_KEY"];
+        SENSOR_ID = config["SENSOR_ID"];
+
+
+        HttpClient http = new HttpClient();
+        http.DefaultRequestHeaders.Add("X-API-KEY", API_KEY);
+        
+        client = new TempStick.TempStickClient(http);
+    }
+
+    [TestMethod]
+    public async Task GetSensor()
+    {
+        var sensor = await client.GetSensorAsync(SENSOR_ID);
+        Assert.IsNotNull(sensor);
+    }
+
+    [TestMethod]
+    public async Task Sensor_Has_Readings()
+    {
+        var reading = await client.GetReadingsAsync(SENSOR_ID);
+        Assert.IsTrue(reading.Type.ToLowerInvariant() == "success");
+    }
+}
